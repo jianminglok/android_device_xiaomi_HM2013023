84da253bdeccf2110835d4ce37990464d0b0fba1
Mansiu3000 <bjsiu0617@gmail.com>
2017-06-26 22:07:31 +0800

    Update Android.mk

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),mt6582)
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),mt6589)
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
@@ -19,7 +19,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := lights.c
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_MODULE := lights.mt6582
+LOCAL_MODULE := lights.mt6589
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
