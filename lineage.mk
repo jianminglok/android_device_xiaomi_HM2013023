ffc47a2bb97a2c34f8736230e443321acf562469
Mansiu3000 <bjsiu0617@gmail.com>
2017-06-27 11:36:29 +0800

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -9,9 +9,9 @@ $(call inherit-product, device/xiaomi/HM2013023/device_HM2013023.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := HM2013023
-PRODUCT_NAME := lineage_HM2013023
+PRODUCT_NAME := lineage_2013023
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := HM2013023
+PRODUCT_MODEL := 2013023
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/2013023/HM2013023:4.4.2/HM2013023/7.6.8:user/release-keys PRIVATE_BUILD_DESC="2013023-user 4.4.2 HM2013023 7.6.8 release-keys"
